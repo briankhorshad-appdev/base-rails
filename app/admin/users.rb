648e35cffa967d5e9e43fee85dfafc43a0a6d50e
briankhorshad <khorshad@chicagobooth.edu>
2020-03-16 21:11:51 +0000

    Mild User updates
    Updated the homepage and removed add user function. The indirect/direct associations are not working. I think it is because all the tables have not been added.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email, :password_digest, :username
-
+  permit_params :email, :password_digest, :username  
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
