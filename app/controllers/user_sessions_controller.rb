193e77362a1fb3a01b3fff6b79b61b229bd1fe67
briankhorshad <khorshad@chicagobooth.edu>
2020-03-19 00:01:35 +0000

    Sorted most user related things.
    Moving on to follow function

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -30,5 +30,14 @@ class UserSessionsController < ApplicationController
 
     redirect_to("/", { :notice => "Signed out successfully." })
   end
+
+   def current_user= (user)
+        @current_user = user
+    end
+
+    def current_user
+        remember_token = User.digest(cookies[:remember_token])
+        @current_user || User.find_by(remember_token: remember_token)       
+    end
  
 end
